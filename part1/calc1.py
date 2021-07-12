1966dea1300ccc0e5bc390febf29692f3ad56296
Mitchell Peabody <mitchell.peabody@gmail.com>
2021-07-11 22:50:06 -0400

    Work part1 problems

--- part1/calc1.py
+++ part1/calc1.py
@@ -2,10 +2,10 @@
 #
 # EOF (end-of-file) token is used to indicate that
 # there is no more input left for lexical analysis
-INTEGER, PLUS, EOF = 'INTEGER', 'PLUS', 'EOF'
+INTEGER, PLUS, MINUS, EOF = 'INTEGER', 'PLUS', 'MINUS', 'EOF'
 
 
-class Token(object):
+class Token:
     def __init__(self, type, value):
         # token type: INTEGER, PLUS, or EOF
         self.type = type
@@ -19,16 +19,13 @@ class Token(object):
             Token(INTEGER, 3)
             Token(PLUS '+')
         """
-        return 'Token({type}, {value})'.format(
-            type=self.type,
-            value=repr(self.value)
-        )
+        return f'Token({self.type}, {repr(self.value)})'
 
     def __repr__(self):
         return self.__str__()
 
 
-class Interpreter(object):
+class Interpreter:
     def __init__(self, text):
         # client string input, e.g. "3+5"
         self.text = text
@@ -40,46 +37,61 @@ class Interpreter(object):
     def error(self):
         raise Exception('Error parsing input')
 
+    @property
+    def at_end_of_stream(self):
+        return self.pos >= len(self.text)
+
+    @property
+    def current_char(self):
+        if self.at_end_of_stream:
+            return None
+        return self.text[self.pos]
+
     def get_next_token(self):
         """Lexical analyzer (also known as scanner or tokenizer)
 
         This method is responsible for breaking a sentence
         apart into tokens. One token at a time.
         """
-        text = self.text
+        while self.current_char and self.current_char.isspace():
+            self.pos += 1
 
         # is self.pos index past the end of the self.text ?
         # if so, then return EOF token because there is no more
         # input left to convert into tokens
-        if self.pos > len(text) - 1:
+        if self.at_end_of_stream:
             return Token(EOF, None)
-
-        # get a character at the position self.pos and decide
-        # what token to create based on the single character
-        current_char = text[self.pos]
 
         # if the character is a digit then convert it to
         # integer, create an INTEGER token, increment self.pos
         # index to point to the next character after the digit,
         # and return the INTEGER token
-        if current_char.isdigit():
-            token = Token(INTEGER, int(current_char))
+        if self.current_char.isdigit():
+            token_buffer = []
+            while self.current_char and self.current_char.isdigit():
+                token_buffer.append(self.current_char)
+                self.pos += 1
+            token = Token(INTEGER, int("".join(token_buffer)))
+            return token
+
+        if self.current_char == '+':
+            token = Token(PLUS, self.current_char)
             self.pos += 1
             return token
 
-        if current_char == '+':
-            token = Token(PLUS, current_char)
+        if self.current_char == '-':
+            token = Token(MINUS, self.current_char)
             self.pos += 1
             return token
 
         self.error()
 
-    def eat(self, token_type):
+    def eat(self, token_types):
         # compare the current token type with the passed token
         # type and if they match then "eat" the current token
         # and assign the next token to the self.current_token,
         # otherwise raise an exception.
-        if self.current_token.type == token_type:
+        if self.current_token.type in token_types:
             self.current_token = self.get_next_token()
         else:
             self.error()
@@ -95,7 +107,7 @@ class Interpreter(object):
 
         # we expect the current token to be a '+' token
         op = self.current_token
-        self.eat(PLUS)
+        self.eat([PLUS, MINUS])
 
         # we expect the current token to be a single-digit integer
         right = self.current_token
@@ -107,7 +119,10 @@ class Interpreter(object):
         # has been successfully found and the method can just
         # return the result of adding two integers, thus
         # effectively interpreting client input
-        result = left.value + right.value
+        if op.type == PLUS:
+            result = left.value + right.value
+        else:
+            result = left.value - right.value
         return result
 
 
